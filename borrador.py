117a0d32d3e74acc14c3e366d42c777628c21656
Miguel <miguel.ramirez@ohmyfi.com>
2022-04-23 12:04:08 -0500

    validations_body

---
+++ borrador.py
@@ -0,0 +1,10 @@
+
+
+def cosa(**kwargs):
+    print(kwargs)
+d = {
+    'saludo':1,
+    'cosa':3
+}
+
+cosa(**d, amparo = 1)
--- main.py
+++ main.py
@@ -13,13 +13,18 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 # Models
-
+#quiere decir que nos debe enviar un json en ese formato, con esos campos
 class Person(BaseModel):
     first_name : str
     last_name : str
     age : int
     hair_color : Optional[str] = None
     is_married : Optional[bool] = None
+
+class Location(BaseModel):
+    city : str
+    state : str
+    country : str
 
 
 @app.get('/')
@@ -76,6 +81,31 @@ def show_person(
     return {
         person_id : "It exists"
     }
+
+# Validaciones : Request body
+#aqui le estamos diciendo, o le estamos pidiendo al cliente que nos debe ingresar en esta PATH
+#dos json, el de Person y el de Location, como hago para dovolver estos dos en formaton JSON
+#recordemos que fastAPI hace automaticamente las conversiones, nosotros enviamos un json
+#en este caso el de formato person y el otro en el formato location, fastApi lo convierte
+#automaticamente a diccionarios y despues nosotros retornamos el JSON, tambien automaticamente
+#convertido, pero en este caso cuando nosotros queremos combinar dos json, tenemos que hacerlo
+#de forma explicita, no podemos dejar a fastApi que haga el trabajo por si mismo
+#tenemos que hacerlo nosotros
+
+@app.put('/person/{person_id}')
+def update_person(
+        person_id : int = Path(
+            ...,
+            title= 'Person ID',
+            description='This is the person ID',
+            gt=0
+        ),
+        person : Person = Body(...),
+        location : Location = Body(...)
+):
+    results = person.dict() | location.dict()
+    return results
+
 
 
 
